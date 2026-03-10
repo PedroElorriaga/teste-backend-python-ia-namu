db69f50980d111e5cc75aab01318a0822a4df1ee
PedroElorriaga <pedrohes2002@outlook.com>
2026-03-10 19:24:54 -0300

    fix: on_envent was deprecated, handle inssue with lifespan attribute

--- src/main/main.py
+++ src/main/main.py
@@ -1,15 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from src.database.postgres_setting import Base, engine
 from src.modules.users.models.user_model import User
 from src.modules.users.router import user_router
 
 
-app = FastAPI()
-
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(user_router.router)
