ff8d3bec561edb5e1748126ff27f191d7095ce7f
PedroElorriaga <pedrohes2002@outlook.com>
2026-03-11 13:58:38 -0300

    fix: changed NAME column properties

--- src/modules/recommendations/models/recommendation_model.py
+++ src/modules/recommendations/models/recommendation_model.py
@@ -7,7 +7,7 @@ class Recommendation(Base):
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    name = Column(String(50), unique=True, nullable=False)
+    name = Column(String(50), nullable=False)
     description = Column(String(255), nullable=False)
     duration = Column(Float, nullable=False)
     category = Column(String(64), nullable=False)
